9a43e74d31cde63629a2b05039aa57c480fdf668
sebas <sebastianwaltilla@hotmail.com>
2020-03-29 15:15:05 +0200

    product page finished for jerry

---
+++ Lab1/js/skinCareData2.js
@@ -0,0 +1,41 @@
+var skinCareData =  [{
+        "productID": "S0001",
+        "category": "skinCare",
+        "src": "skinCare/product1.jpg",
+        "hyperLink": "productSinglePage.html",
+        "classImg": "product_img",
+        "title": "Post Poo Drops",
+        "infoText": "En aromatisk och botanisk badrumsdeodorant som bekämpar illaluktande. Post-Poo Drops skarpa toner av citrusskal - förstärkta med berikande blomessenser - neutraliserar obehagliga lukter i badrummet på ett högeffektivt vis.",
+        "price": 199,
+        "weight": 50
+    },
+    {
+        "productID": "S0002",
+        "src": "skinCare/product2.jpg",
+        "category": "skinCare",
+        "hyperLink": "productSinglePage.html",
+        "classImg": "product_img",
+        "title": "Post Poo Drops",
+        "infoText": "En aromatisk och botanisk badrumsdeodorant som bekämpar illaluktande. Post-Poo Drops skarpa toner av citrusskal - förstärkta med berikande blomessenser - neutraliserar obehagliga lukter i badrummet på ett högeffektivt vis.",
+        "price": 199
+    },
+    {
+        "productID": "S0003",
+        "category": "skinCare",
+        "src": "skinCare/product3.jpg",
+        "hyperLink": "productSinglePage.html",
+        "classImg": "product_img",
+        "title": "Post Poo Drops",
+        "infoText": "En aromatisk och botanisk badrumsdeodorant som bekämpar illaluktande. Post-Poo Drops skarpa toner av citrusskal - förstärkta med berikande blomessenser - neutraliserar obehagliga lukter i badrummet på ett högeffektivt vis.",
+        "price": 234
+    },
+    {
+        "productID": "S0004",
+        "category": "skinCare",
+        "src": "skinCare/product4.jpg",
+        "hyperLink": "productSinglePage.html",
+        "classImg": "product_img",
+        "title": "Post Poo Drops",
+        "infoText": "En aromatisk och botanisk badrumsdeodorant som bekämpar illaluktande. Post-Poo Drops skarpa toner av citrusskal - förstärkta med berikande blomessenser - neutraliserar obehagliga lukter i badrummet på ett högeffektivt vis.",
+        "price": 435
+    }]
