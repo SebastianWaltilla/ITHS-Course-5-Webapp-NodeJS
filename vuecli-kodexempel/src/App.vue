4fc7956047630f89ec8fe7ee033498338e1be1bc
sebas <sebastianwaltilla@hotmail.com>
2020-05-05 23:38:53 +0200

    started some vue projects from jerry

---
+++ vuecli-kodexempel/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+
+      <img alt="Vue logo" src="./assets/logo.png">
+      <h1>{{helloMessage}}</h1>
+      <HelloWorld msg="Welcome to Your Vue.js App"></HelloWorld>
+      <MyList></MyList>
+      <MyBooks></MyBooks>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MyBooks from "./components/MyBooks";
+import MyList from "./components/MyList";
+
+export default {
+  name: 'App',
+  components: {
+      HelloWorld,
+      MyBooks,
+      MyList
+  },
+    data: function(){
+        return {
+           helloMessage: "Hello"
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10px;
+}
+</style>
---
+++ vuecli-kodexempel/src/components/MyList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h2>Min lista</h2>
+        <!--<img alt="Vue logo" src="../assets/logo.png">-->
+        <ul>
+            <li v-for="post in posts" v-bind:key="post.id">{{ post.title }} {{ post.content }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MyList",
+        data: function(){
+            return {
+            posts: [
+                { id: 1, title: 'My journey with Vue', content: 'My jorney .....' },
+                { id: 2, title: 'Blogging with Vue', content: 'Blogging ......' },
+                { id: 3, title: 'Why Vue is so fun', content: 'Why Vue .....' }
+            ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+h2{
+    font-family: Georgia, Helvetica, Arial, san-serif;
+}
+</style>
